92cd7891f577a9f76d3233f86c6bef98b6253332
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2024-01-07 22:21:41 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -101,7 +101,8 @@ def create_app(settings: Settings):
     app.openapi = custom_openapi(app)
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,
+        allow_origins=["*"], 
+        # origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
