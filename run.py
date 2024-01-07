cb407c36948605c24aa17d332bd1ff07a1e3abd9
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2024-01-07 10:27:29 +0100

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-from api.database import create_db_and_tables, create_town_and_people, get_db
-
-
-if __name__=="__main__":
-    db = next(get_db())  # Fetching the database session
-    create_db_and_tables()
-
-    create_town_and_people(db)
