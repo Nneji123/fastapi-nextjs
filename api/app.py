2cfe6531ba22d7084a22f9b7b760d1883e778c65
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2024-01-07 22:31:36 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -13,54 +13,7 @@ from api.public.people.urls import people_router
 
 
 from api.database import create_db_and_tables, create_town_and_people, get_db
-
-
-
-lable_lang_mapping = {"Plain JS": "JavaScript", "NodeJS": "JavaScript"}
-
-def add_examples(openapi_schema: dict, docs_dir):
-    path_key = 'paths'
-    code_key = 'x-codeSamples'
-
-    for folder in os.listdir(docs_dir):
-        base_path = os.path.join(docs_dir, folder)
-        files = [f for f in os.listdir(base_path) if os.path.isfile(os.path.join(base_path, f))]
-        for f in files:
-            parts = f.split('-')
-            if len(parts) >= 2:
-                route = '/' + '/'.join(parts[:-1])
-                method = parts[-1].split('.')[0]
-                print(f'[{path_key}][{route}][{method}][{code_key}]')
-
-                if route in openapi_schema[path_key]:
-                    if code_key not in openapi_schema[path_key][route][method]:
-                        openapi_schema[path_key][route][method].update({code_key: []})
-
-                    openapi_schema[path_key][route][method][code_key].append({
-                        'lang': lable_lang_mapping[folder],
-                        'source': open(os.path.join(base_path, f), "r").read(),
-                        'label': folder,
-                    })
-            else:
-                print(f'Error in adding examples code to openapi {f}')
-
-    return openapi_schema
-
-
-def custom_openapi(app):
-    if app.openapi_schema:
-        return app.openapi_schema
-
-    openapi_schema = get_openapi()
-    openapi_schema["info"]["x-logo"] = {
-        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
-    }
-
-    app.openapi_schema = add_examples(openapi_schema, 'docs')
-
-    return app.openapi_schema
-
-
+from api.utils import *
 
 
 
@@ -93,7 +46,7 @@ def create_app(settings: Settings):
         description=settings.DESCRIPTION,
         lifespan=lifespan,
     )
-    app.openapi = custom_openapi(app)
+    # app.openapi = custom_openapi(app)
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"], 
