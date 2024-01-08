c07ee8d411c7ac3f5f161c3cb9965bf3ec298871
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2024-01-08 12:49:23 +0000

    updated init_db.py

--- api/init_db.py
+++ api/init_db.py
@@ -3,6 +3,16 @@ from sqlalchemy.orm import sessionmaker
 from sqlmodel import Session, SQLModel, create_engine
 from sqlmodel.ext.asyncio import session
 
+import sys
+from pathlib import Path
+
+# Get the absolute path of the directory containing this script
+current_file = Path(__file__).resolve()
+parent_directory = current_file.parent
+project_directory = parent_directory.parent
+
+sys.path.insert(0, str(project_directory))
+
 from api.database import create_db_and_tables, create_town_and_people, get_db
 
 
